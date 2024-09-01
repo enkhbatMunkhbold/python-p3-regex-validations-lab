83849e757a5aa840fc8fc421f7195315b34beef6
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-09-01 13:41:51 -0700

    Complete lab

--- lib/regex.py
+++ lib/regex.py
@@ -3,10 +3,12 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"[A-Z][']*([A-Za-z][ \'-]{0,1})+"
+# name = r"\w+\s\w+\W\w+"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"([(]*[0-9]{3}[ )-]*)+[0-9]+gm"
+# phone_number = r"[(]*[0-9]{3}[ )-]*"
 phone_regex = re.compile(phone_number)
 
 email_address = r""
