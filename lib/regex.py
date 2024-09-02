8207fc9c9e38334af9066c319dd90f49de79b554
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-09-01 18:30:17 -0700

    Complete lab

--- lib/regex.py
+++ lib/regex.py
@@ -4,12 +4,10 @@ import re
 # Feel free to encourage students to find oversights and add tests to this lab!
 
 name = r"[A-Z][']*([A-Za-z][ \'-]{0,1})+"
-# name = r"\w+\s\w+\W\w+"
 name_regex = re.compile(name)
 
-phone_number = r"([(]*[0-9]{3}[ )-]*)+[0-9]+gm"
-# phone_number = r"[(]*[0-9]{3}[ )-]*"
+phone_number = r"([(]*[0-9]{3}[ )]*[-]{0,1})+([0-9]{4})+"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"([A-z]\w+[.]*\w+)@[\w]+[.]com"
 email_regex = re.compile(email_address)
